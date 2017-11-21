0cb0c99c76a6f905fb9fcb0c3739e557eb990b8a
Sattar <settar216@gmail.com>
2017-11-21 12:49:06 +0400

    popup effects added. icons added. Probably completed

--- js/script.js
+++ js/script.js
@@ -4,9 +4,84 @@ $(document).ready(function(){
         $(this).addClass("active");
     });
     
+    // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 70
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash - 70;
+      });
+    } // End if
+  });
+    
+    
+    
+    
+    
+    
+    $(window).scroll(function () {
+        var window_width = $( window ).width();
+        if ( window_width > 975) {
+
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop < 100) {
+                maxHeight = 150;
+            } else if (scrollTop > 400) {
+                maxHeight = 75;
+            } else {
+                maxHeight = 150 - 75 * ((scrollTop - 200)) / 200;
+            }
+            $('nav').css({
+                'max-height': maxHeight + "px"
+            });
+        } 
+    });
+
+
+    
+    $('.cd-gallery .popup-image').magnificPopup({
+      type: 'image',
+      mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+        
+        
+    });
+
+    
     
 });
-
+    
+    
+    
+    
 function initMap() {
     var uluru = {lat: 40.4192291, lng: 49.8185043};
     var map = new google.maps.Map(document.getElementById('map'), {
